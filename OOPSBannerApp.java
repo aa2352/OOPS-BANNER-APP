f8cd3c2abc1bcdaecfd8bafadb8dba0cd9684fe0
Akshay <aa2352@srmist.edu.in>
2026-02-21 20:34:43 +0530

    Rendered OOPS as an ASCII Banner for UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,25 @@
+/**
+ * @author Akshay
+ * @version 2
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Goal: Print OOPS as a banner using string concatenation and multiple print statements
+        
+        // Line 1
+        System.out.println("   *** " + " " + "   *** " + " " + " ****** " + " " + "  ***** ");
+        // Line 2
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ** ");
+        // Line 3
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** ** " + " " + " ** ");
+        // Line 4
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ****** " + " " + "  ***** ");
+        // Line 5
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "      ** ");
+        // Line 6
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + " ** ** ");
+        // Line 7
+        System.out.println("   *** " + " " + "   *** " + " " + " ** " + " " + "  ***** ");
     }
 }
